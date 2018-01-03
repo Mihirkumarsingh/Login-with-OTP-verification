7b309764a9aec825668d5a355ba86d193bbc211f
Mihirkumarsingh <singhmihirkumar@gmail.com>
2018-01-04 01:52:55 +0530

    header and footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+ <footer  id="footer1">
+ 
+
+   <div class="mx-auto " style="width:270px;">Copyright @ Mihir Kumar Singh</div>
+   
+
+</footer>       
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+      
+
+    </body>
+  </html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+     <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+       <nav id="nav1" class="navbar navbar-expand-md  navbar-dark" >
+  <!-- Brand -->
+  <a class="navbar-brand" href="#" style=" background-color:#26a69a;">Sahyog</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar" style=" background-color:#26a69a;">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li> 
+      <div id="google_translate_element" style="padding-top: 7px;"></div><script type="text/javascript">
+       function googleTranslateElementInit() {
+         new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,hi', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    </ul>
+  </div> 
+</nav>
